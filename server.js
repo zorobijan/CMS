6476b3cd7f106d60511d8a3c64b42da121298e79
Matthew Bascom <matt.bijan@gmail.com>
2022-10-04 23:44:24 -0500

    created response functions

--- server.js
+++ server.js
@@ -31,33 +31,89 @@ async function main() {
             'Update Employee Role',
             'Exit'
       }
-      {
-        type: 'input',
-        name: 'last_name',
-        message: "What's your last name",
-        default() {
-          return 'Doe';
-        },
+      if (responseObject.action === 'View Departments') {
+        viewDepartments()
       }
-    ])
+      else if (responseObject.action === 'View Roles') {
+        viewRoles()
+      }
+      else if (responseObject.action === 'View Employees') {
+        viewEmployees()
+      }
+      else if (responseObject.action === 'Add Departments') {
+        addDepartment()
+      }
+      else if (responseObject.action === 'Add Roles') {
+        addRole()
+      }
+      else if (responseObject.action === 'Add Employees') {
+        addEmployee()
+      }
+      else if (responseObject.action === 'Update Employee Role') {
+        updateEmployeeRole()
+      }
+])
 
       console.log(responseObject)
-
-
+} 
     // query database
-    const [rows] = await connection.execute(`SELECT * FROM employees where firstname = ?`,[responseObject.first_name] );
+async function viewDepartments () {const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] )
+  }; 
+async function viewRoles () {const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] )
+}; 
+async function viewEmployees () {const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] )
+}; 
+async function addDepartment () {const [rows] = await connection.execute(`INSERT INTO department SET ?`, {
+  departmentName: responseObject.
+}
+}; 
+async function addRole () {const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] )
+}; 
+async function addEmployee () {const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] )
+}; 
+async function updateEmployee () {const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] )
+}; 
+
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM roles`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM employees`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM departments`,[responseObject.action] );
+    console.table(rows)
+    const [rows] = await connection.execute(`SELECT * FROM roles where employee title = ?`,[responseObject.employee_title] );
+    const [rows] = await connection.execute(`SELECT * FROM employees`,[responseObject.] );
+    const [rows] = await connection.execute(`SELECT * FROM roles where firstname = ?`,[responseObject.first_name] );
+    const [rows] = await connection.execute(`SELECT * FROM roles where firstname = ?`,[responseObject.first_name] );
+
+
+    await inquirer.prompt([ {
+      type: 'list',
+      name: 'action',
+      message: 'Please select one of the following options',
+      choices: [
+          'View Departments',
+          'View Roles',
+          'View Employees',
+          'Add Departments',
+          'Add Roles',
+          'Add Employees',
+          'Update Employee Role',
+          'Exit'
+    }
 
 
 
   }
 
-async function 
-    const responseObject = await inquirer.prompt([ {
-       
-    }])
-    console.log(results);
-    const [rows] = await connection.execute(`SELECT * FROM employees where first_name = ?`,[responseObject.first_name] );
-    console.log(results);
 
 
     
